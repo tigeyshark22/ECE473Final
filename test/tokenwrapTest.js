a059a8995f019975297f00ca25287cf6759b0774
claireshin86 <124474086+claireshin86@users.noreply.github.com>
2024-04-10 21:12:57 -0400

    Update tokenwrapTest.js

--- test/tokenwrapTest.js
+++ test/tokenwrapTest.js
@@ -5,6 +5,7 @@ contract("ERC20Wrapper", (accounts) => {
     let lethInstance;
     let wrapperInstance;
     const initialLETHSupply = web3.utils.toBN(web3.utils.toWei('1000', 'ether'));
+    const depositAmount = web3.utils.toBN(web3.utils.toWei('100', 'ether'));
 
     before(async () => {
         wrapperInstance = await ERC20Wrapper.new(lethInstance.address);
@@ -13,19 +14,24 @@ contract("ERC20Wrapper", (accounts) => {
     });
 
     it("Test 1: Test Deposit", async () => {
-        const depositAmount = web3.utils.toWei('10', 'ether');
+        const initialBalance = await wrapperInstance.balanceOf(user);
+        await lethInstance.approve(wrapperInstance.address, depositAmount, { from: user });
+        await wrapperInstance.deposit(depositAmount, { from: user });
+        const finalLETHBalance = await lethInstance.balanceOf(user);
+        const finalWLETHBalance = await wrapperInstance.balanceOf(user);
         
-        await wrapperInstance.deposit(depositAmount, {from: accounts[1]});
-
-        const finalLETHBalance = await lethInstance.balanceOf(accounts[1]);
-        const finalWLETHBalance = await wrapperInstance.balanceOf(accounts[1]);
-
+        assert.isTrue(finalWLETHBalance.gt(initialBalance), 'Wrapped token balance should increase after deposit');
         assert.equal(finalLETHBalance.toString(), initialLETHSupply.sub(web3.utils.toBN(depositAmount)).toString(), "LETH balance should decrease by deposit amount");
         assert.equal(finalWLETHBalance.toString(), depositAmount, "WLETH balance should be equal to deposit amount");
     });
 
     it("Test 2: Test Withdraw", async () => {
         const withdrawAmount = web3.utils.toWei('5', 'ether');
+        await lethInstance.approve(wrapperInstance.address, depositAmount, { from: user });
+        await wrapperInstance.deposit(depositAmount, { from: user });
+        const initialBalance = await wrapperInstance.balanceOf(user);
+        await wrapperInstance.withdraw(depositAmount, { from: user });
+        const finalBalance = await wrapperInstance.balanceOf(user);
 
         await wrapperInstance.withdraw(withdrawAmount, {from: accounts[1]});
 
