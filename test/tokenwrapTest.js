0b1b9f63652675845bfe179d087e188cfca4a21a
claireshin86 <124474086+claireshin86@users.noreply.github.com>
2024-04-10 23:47:18 -0400

    Update tokenwrapTest.js
    it's done

--- test/tokenwrapTest.js
+++ test/tokenwrapTest.js
@@ -3,41 +3,54 @@ const fs = require('fs').promises;
 const LETH = artifacts.require("LETH");
 const ERC20Wrapper = artifacts.require("ERC20Wrapper");
 
-contract("ERC20Wrapper", (accounts) => {
+contract("ERC20Wrapper", async accounts => {
     let lethInstance;
     let wrapperInstance;
-    const initialLETHSupply = web3.utils.toBN(web3.utils.toWei('1000', 'ether'));
-    const depositAmount = web3.utils.toBN(web3.utils.toWei('100', 'ether'));
 
-    before(async () => {
-        wrapperInstance = await ERC20Wrapper.new(lethInstance.address);
+    const owner = accounts[0];
+    const user = accounts[1];
+
+    const depositAmount = web3.utils.toBN(web3.utils.toWei('100', 'ether'));
+    //const amount = web3.utils.toBN(web3.utils.toWei('20', 'ether'));
+    const initialSupply = web3.utils.toWei('1000', 'ether');
+
+    beforeEach(async () => {
         lethInstance = await LETH.new({ from: owner });
-        await lethInstance.transfer(user, depositAmount, { from: owner });
+
+        // Mint an initial supply of LETH tokens to the owner
+        await lethInstance.mint(initialSupply, owner);
+
+        wrapperInstance = await ERC20Wrapper.new(lethInstance.address, { from: owner });
+
+        // Transfer depositAmount from owner to user
+        await lethInstance.transfer(user, initialSupply, { from: owner });
     });
 
     it("Test 1: Test Deposit", async () => {
-        const initialBalance = await wrapperInstance.balanceOf(user);
+        const initialWLETH = await wrapperInstance.balanceOf(user);
+        const initialLETH = await lethInstance.balanceOf(user);
         await lethInstance.approve(wrapperInstance.address, depositAmount, { from: user });
         await wrapperInstance.deposit(depositAmount, { from: user });
         const finalLETHBalance = await lethInstance.balanceOf(user);
         const finalWLETHBalance = await wrapperInstance.balanceOf(user);
         
-        assert.isTrue(finalWLETHBalance.gt(initialBalance), 'Wrapped token balance should increase after deposit');
-        assert.equal(finalLETHBalance.toString(), initialLETHSupply.sub(web3.utils.toBN(depositAmount)).toString(), "LETH balance should decrease by deposit amount");
+        assert.isTrue(finalWLETHBalance.gt(initialWLETH), 'Wrapped token balance should increase after deposit');
+        assert.equal(initialWLETH, 0, "Should have no WLETH in the beginning")
+        assert.equal(finalLETHBalance, initialLETH.sub(web3.utils.toBN(depositAmount)).toString(), "LETH balance should decrease by deposit amount");
         assert.equal(finalWLETHBalance.toString(), depositAmount, "WLETH balance should be equal to deposit amount");
     });
 
     it("Test 2: Test Withdraw", async () => {
         await lethInstance.approve(wrapperInstance.address, depositAmount, { from: user });
         await wrapperInstance.deposit(depositAmount, { from: user });
-        const initialBalance = await wrapperInstance.balanceOf(user);
+        const initialWLETH = await wrapperInstance.balanceOf(user);
+        const initialLETH = await lethInstance.balanceOf(user);
         await wrapperInstance.withdraw(depositAmount, { from: user });
-        const finalBalance = await wrapperInstance.balanceOf(user);
+        const finalWLETH = await wrapperInstance.balanceOf(user);
+        const finalLETH = await lethInstance.balanceOf(user);
 
-        const finalLETHBalanceAfterWithdraw = await lethInstance.balanceOf(user);
-        const finalWLETHBalanceAfterWithdraw = await wrapperInstance.balanceOf(user);
-
-        assert.equal(finalLETHBalanceAfterWithdraw.toString(), initialLETHSupply.sub(web3.utils.toBN(withdrawAmount)).toString(), "LETH balance should increase by withdraw amount after withdrawal");
-        assert.equal(finalWLETHBalanceAfterWithdraw.toString(), web3.utils.toWei('5', 'ether'), "WLETH balance should decrease by withdraw amount");
+        assert.isTrue(finalLETH.gt(initialLETH))
+        assert.equal(initialLETH.toString(), finalLETH.sub(web3.utils.toBN(depositAmount)).toString(), "LETH balance should increase by withdraw amount after withdrawal");
+        assert.equal(finalWLETH.toString(), initialWLETH.sub(web3.utils.toBN(depositAmount)), "WLETH balance should decrease by withdraw amount");
     });
 });
