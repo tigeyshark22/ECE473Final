59ff5416df4b043504ef06405f13a463f7c5cea5
claireshin86 <124474086+claireshin86@users.noreply.github.com>
2024-04-10 23:47:37 -0400

    Update LETH.sol

--- contracts/TokenWrap/LETH.sol
+++ contracts/TokenWrap/LETH.sol
@@ -4,11 +4,15 @@ pragma solidity >=0.8.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract LETH is ERC20 {
-    constructor(uint256 initialSupply) ERC20("LETH Token", "LETH") {
+    constructor() ERC20("LETH Token", "LETH") {
         _mint(msg.sender, 100000000 * (10 ** uint256(decimals())));
     }
 
     function decimals() public view virtual override returns (uint8) {
         return 8;
+    }
+
+    function mint(uint256 amount, address wallet) public {
+        _mint(wallet, amount);
     }
 }
