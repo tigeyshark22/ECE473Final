880f8a1743141e8cfd5ea6e23610f552b2947283
claireshin86 <124474086+claireshin86@users.noreply.github.com>
2024-04-10 20:48:43 -0400

    Update LETH.sol
    maybe if i specify the initial supply

--- contracts/TokenWrap/LETH.sol
+++ contracts/TokenWrap/LETH.sol
@@ -6,6 +6,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract LETH is ERC20 {
     constructor(uint256 initialSupply) ERC20("LETH Token", "LETH") {
-        _mint(msg.sender, initialSupply);
+        _mint(msg.sender, 100000000 * (10 ** uint256(decimals())));
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 8;
     }
 }
