9f0e3bfccf689531d1bb966e21acee2d96c9ce88
tigeyshark22 <yang_o@yahoo.com>
2024-04-11 22:16:09 -0400

    oops i didn't mean to add that

--- migrations/1_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-const LUSD = artifacts.require("LUSD"); 
-const TIGER = artifacts.require("TIGER"); 
-const ARB = artifacts.require("Arbitrage");
-module.exports = function (deployer) { 
-  deployer.deploy(LUSD);
-  deployer.deploy(TIGER);
-  deployer.deploy(ARB);
-}; 
