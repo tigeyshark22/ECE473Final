34472be7661b880620af3c2663c09914885a09af
tigeyshark22 <yang_o@yahoo.com>
2024-05-07 13:39:14 -0400

    fixed simple contract

--- contracts/SimpleContract/SimpleContract.sol
+++ contracts/SimpleContract/SimpleContract.sol
@@ -3,7 +3,8 @@ pragma solidity>=0.7.0 <0.9.0;
 
 contract simpleContract{
 	string test;
-	function simpleContractfunc(string memory _init) public {
+
+	constructor(string memory _init) {
 		test = _init;
 	}
 	function setTest(string memory _newTest) public {
